2e4ca0a5e7bdb4312c13051282c645b337e4b62e
Sergey_Ionov <ions1168@gmail.com>
2025-10-28 17:05:49 +0500

    task3

--- anfisa_for_friends/contest/views.py
+++ anfisa_for_friends/contest/views.py
@@ -1,5 +1,10 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
+from .forms import ContestForm
 
-def proposal(request):
-    return render(request, 'contest/form.html')
+
+def proposal(request: HttpRequest):
+    form = ContestForm(request.GET or None)
+    context = {'form': form}
+    return render(request, 'contest/form.html', context)
