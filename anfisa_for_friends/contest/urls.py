4ee49ec2881dcfa7b43fb4faa61a0dd3d09e9822
Sergey_Ionov <ions1168@gmail.com>
2025-10-29 01:06:27 +0500

    task8-prep

--- anfisa_for_friends/contest/urls.py
+++ anfisa_for_friends/contest/urls.py
@@ -6,5 +6,6 @@ app_name = 'contest'
 
 urlpatterns = [
     path('', views.proposal, name='create'),
-    path('list/', views.proposal_list, name='list')
+    path('list/', views.proposal_list, name='list'),
+    # Добавьте сюда новые маршруты для редактирования и удаления заявок.
 ]
--- anfisa_for_friends/contest/views.py
+++ anfisa_for_friends/contest/views.py
@@ -5,14 +5,21 @@ from .models import Contest
 
 
 def proposal(request):
+    # Допишите функцию, чтобы она могла работать как на создание заявки,
+    # так и на редактирование.
     form = ContestForm(request.POST or None)
+    context = {'form': form}
     if form.is_valid():
         form.save()
-    context = {'form': form}
     return render(request, 'contest/form.html', context)
 
 
+def delete_proposal(request):
+    # Допишите функцию для удаления заявок.
+    return
+
+
 def proposal_list(request):
-    proposals = Contest.objects.all().order_by('id')
-    context = {'proposals': proposals}
+    contest_proposals = Contest.objects.order_by('id')
+    context = {'contest_proposals': contest_proposals}
     return render(request, 'contest/contest_list.html', context)
