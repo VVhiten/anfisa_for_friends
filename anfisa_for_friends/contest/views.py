6cdc069f606936683660c10ddb3361155e9a8491
Sergey_Ionov <ions1168@gmail.com>
2025-10-29 01:31:27 +0500

    task8

--- anfisa_for_friends/contest/views.py
+++ anfisa_for_friends/contest/views.py
@@ -1,22 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .forms import ContestForm
 from .models import Contest
 
 
-def proposal(request):
-    # Допишите функцию, чтобы она могла работать как на создание заявки,
-    # так и на редактирование.
-    form = ContestForm(request.POST or None)
+def proposal(request, pk=None):
+    if pk is None:
+        instance = None
+    else:
+        instance = get_object_or_404(Contest, pk=pk)
+    form = ContestForm(request.POST or None, instance=instance)
     context = {'form': form}
     if form.is_valid():
         form.save()
     return render(request, 'contest/form.html', context)
 
 
-def delete_proposal(request):
-    # Допишите функцию для удаления заявок.
-    return
+def delete_proposal(request, pk):
+    instance = get_object_or_404(Contest, pk=pk)
+    if request.method == 'POST':
+        instance.delete()
+        return redirect('contest:list')
+    form = ContestForm(instance=instance)
+    context = {'form': form}
+    return render(request, 'contest/form.html', context)
 
 
 def proposal_list(request):
