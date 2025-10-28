bc33a387a8229fc86f80f63a1deefa3afb85c80d
Sergey_Ionov <ions1168@gmail.com>
2025-10-28 19:36:44 +0500

    task5

--- anfisa_for_friends/contest/models.py
+++ anfisa_for_friends/contest/models.py
@@ -1,3 +1,16 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Опишите модель Contest здесь!
+
+class Contest(models.Model):
+    title = models.CharField(verbose_name='Название', max_length=20)
+    description = models.TextField(verbose_name='Описание')
+    price = models.IntegerField(
+        verbose_name='Цена',
+        help_text='Рекомендованная розничная цена',
+        validators=[MinValueValidator(10), MaxValueValidator(100)],
+    )
+    comment = models.TextField(
+        verbose_name='Комментарий',
+        blank=True,
+    )
