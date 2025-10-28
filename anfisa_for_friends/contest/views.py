df99b3c4967051e1c45d1533311365644ee3d75f
Sergey_Ionov <ions1168@gmail.com>
2025-10-28 16:50:05 +0500

    task3-prep

--- anfisa_for_friends/contest/views.py
+++ anfisa_for_friends/contest/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
 
 
-def proposal_create(request):
+def proposal(request):
     return render(request, 'contest/form.html')
-
-
-def accepted(request):
-    return render(request, 'contest/proposal_accepted.html')
