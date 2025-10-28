8ba03e6bcf8d7a419f1691519e5ddc88c7fe7551
Sergey_Ionov <ions1168@gmail.com>
2025-10-28 17:35:33 +0500

    task4

--- anfisa_for_friends/anfisa_for_friends/settings.py
+++ anfisa_for_friends/anfisa_for_friends/settings.py
@@ -15,6 +15,7 @@ INSTALLED_APPS = [
     'ice_cream.apps.IceCreamConfig',
     'about.apps.AboutConfig',
     'contest.apps.ContestConfig',
+    'django_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
